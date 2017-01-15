883979b19649393a7e73024891440b1ed5c3a0bc
amorganPD <amorgan@prioritydesigns.com>
2017-01-15 01:19:17 +0000

    Changed IP

--- action.js
+++ action.js
@@ -1,10 +1,10 @@
 $("#forward").click(function(){
-  $.get("http://192.168.1.91:3000/?direction=forward", function(data, status){
+  $.get("http://192.168.1.92:3000/?direction=forward", function(data, status){
         alert("Full steam ahead!");
     });
 });
 $("#backward").click(function(){
- $.get("http://192.168.1.91:3000/?direction=backward", function(data, status){
+ $.get("http://192.168.1.92:3000/?direction=backward", function(data, status){
         alert("Take it back now, y'all");
     });
 });
