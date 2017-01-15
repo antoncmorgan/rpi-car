2afa726c9971fb4bc874c4da9b6b82efe265b921
Jacob Rutter <rutter.jacob@gmail.com>
2017-01-14 20:33:10 -0500

    Added a Stop button and jquery click function

--- action.js
+++ action.js
@@ -1,10 +1,12 @@
 $("#forward").click(function(){
   $.get("http://192.168.1.92:3000/?direction=forward", function(data, status){
-        alert("Full steam ahead!");
     });
 });
 $("#backward").click(function(){
  $.get("http://192.168.1.92:3000/?direction=backward", function(data, status){
-        alert("Take it back now, y'all");
+    });
+});
+$("#stop").click(function(){
+ $.get("http://192.168.1.92:3000/?direction=stop", function(data, status){
     });
 });
