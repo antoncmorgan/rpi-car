abf65d363d0c2434995011125344a8d80b5420b3
amorganPD <amorgan@prioritydesigns.com>
2017-01-15 01:03:59 +0000

    First control from api of car
    - Modified index.js to use uri to change direction (forward/stop)
    - Added python-shell dependency (npm install python-shell --save)

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express')
 var url = require('url');
+var python = require('python-shell');
 var app = express()
+
+//args: Right (0-255), Left (0-255), Direction (1: forward, 0: background)
+var pyForward = {
+  args: ['40','40','1']
+}
+var pyBackward = {
+  args: ['0','0','0']
+}
 
 app.get('/', function (req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   var direction = req.query.direction;
+  if (direction == 'forward') {
+    python.run('./python/DCMotors.py', pyForward, function () {
+      console.log("Going Forward");
+    });
+  }
+  else if (direction == 'backward') {
+    python.run('./python/DCMotors.py', pyBackward, function () {
+      console.log("Going Backwards");
+    });
+  }
   res.send(direction);
 })
 
